4db589b5cf5034dca9047426f1cb27c4f85d38f8
marcos7-eri <choqueerick249@gmail.com>
2025-09-18 18:32:12 -0400

    10 commit

---
+++ pedidos/pedidos/Migrations/20231015000000_InitialCreate.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pedidos.Migrations
+{
+    public class _20231015000000_InitialCreate : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
